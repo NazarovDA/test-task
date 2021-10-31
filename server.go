a6d8ec184f342d197cec4de6e5fbd9e7859e75d4
NazarovDA <d.n.0@bk.ru>
2021-10-31 18:02:39 +0300

    -

--- server.go
+++ server.go
@@ -215,7 +215,7 @@ func echoHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	port := "8080"
+	port := os.Getenv("PORT")
 	fmt.Print(port)
 	http.HandleFunc("/echo", echoHandler)
 	http.Handle("/", http.FileServer(http.Dir(".")))
